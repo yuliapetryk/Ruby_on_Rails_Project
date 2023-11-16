270fa5c6140d72abd7a42d925f468c088078aae8
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-16 13:51:29 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "parcels#index"
   get 'info' => 'info#list', as: 'list'
-  get "/parcels", to: "parcels#index"
+  get "/parcels" =>  "parcels#index", as: 'index'
   get "/info/from" => "info#showFrom", as: 'showFrom'
   get "/info/value" => "info#showValue", as:'showValue'
   get "/info/repeated" => "info#showRepeated", as: 'showRepeated'
